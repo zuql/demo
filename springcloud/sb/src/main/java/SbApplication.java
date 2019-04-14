41ea356d27b2176304f01d788e8de36d20737e3b
zuql <17712743419@163.com>
2019-04-14 21:17:24 +0800

    提交

---
+++ springcloud/sb/src/main/java/SbApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbApplication.class, args);
+	}
+
+}
